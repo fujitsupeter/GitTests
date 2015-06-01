44c038ee8ebc5ad0d0ffd72798a98c3f5616f971
Peter Carlsson <peter@liteolika.se>
2015-06-01 21:19:54 +0200

    Added logger class and tests

---
+++ Src/Logging.Tests/LoggerTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logging.Tests
+{
+    [TestFixture]
+    public class LoggerTests
+    {
+        [Test]
+        public void Log_Should_Return_Logstring()
+        {
+            var expect = "LOG: HELLO WORLD!";
+
+            var actual = Logger.Log("HELLO WORLD!");
+
+            Assert.AreEqual(expect, actual);
+
+        }
+
+    }
+}
